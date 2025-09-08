ce0b575c0b990fc74aa3e49856aa39df62b670b5
Вячеслав Нестеренко <Tyuiu.NesterenkoVV@outlook.com>
2025-09-08 21:10:20 +0500

    Task2

---
+++ Tyuiu.NesterenkoVV.Sprint0.Task2.V0.Test/DataServiceTest.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+
+using Tyuiu.NesterenkoVV.Sprint0.Task2.V0.Lib;
+
+namespace Tyuiu.NesterenkoVV.Sprint0.Task2.V0.Test
+{
+    [TestClass]
+    public class DataServiceTest
+    {
+        [TestMethod]
+        public void CheckGetMessageValid()
+        {
+            var name = "Вячеслав";
+            var res = DataService.GetMessage(name);
+            Assert.AreEqual("Привет, Вячеслав", res);
+        }
+    }
+}
