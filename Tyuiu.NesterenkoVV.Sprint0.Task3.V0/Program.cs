262f8dedf440d0cf56ab15936df038fdb2dfcf3f
Вячеслав Нестеренко <Tyuiu.NesterenkoVV@outlook.com>
2025-09-11 13:11:13 +0500

    Task3

---
+++ Tyuiu.NesterenkoVV.Sprint0.Task3.V0/Program.cs
@@ -0,0 +1,19 @@
+﻿
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Tyuiu.NesterenkoVV.Sprint0.Task3.V0.Lib;
+namespace Tyuiu.NesterenkoVV.Sprint0.Task3.V0
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(DataService.Sum(5, 12));
+            Console.ReadKey();
+        }
+    }
+}
