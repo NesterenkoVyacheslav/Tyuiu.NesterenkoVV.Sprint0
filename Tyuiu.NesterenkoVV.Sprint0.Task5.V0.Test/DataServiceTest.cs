51faf6179f0ae9bc9ab1f7c5b5627bb5c3809d77
Вячеслав Нестеренко <Tyuiu.NesterenkoVV@outlook.com>
2025-09-14 13:08:15 +0500

    Task5

---
+++ Tyuiu.NesterenkoVV.Sprint0.Task5.V0.Test/DataServiceTest.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Tyuiu.NesterenkoVV.Sprint0.Task5.V0.Lib;
+
+namespace Tyuiu.NesterenkoVV.Sprint0.Task5.V0.Test
+{
+    [TestClass]
+    public sealed class DataServiceTest
+    {
+        [TestMethod]
+        public void CheckedAdditionValid()
+        {
+            Assert.AreEqual(10, DataService.Addition(5, 5));
+        }
+        [TestMethod]
+        public void CheckedSubstractionValid()
+        {
+            Assert.AreEqual(5, DataService.Substraction(10, 5));
+        }
+        [TestMethod]
+        public void CheckedMultiplicationValid()
+        {
+            Assert.AreEqual(25, DataService.Multiplication(5, 5));
+        }
+        [TestMethod]
+        public void CheckedDivisionValid()
+        {
+            Assert.AreEqual(1, DataService.Division(5, 5));
+        }
+    }
+}
---
+++ Tyuiu.NesterenkoVV.Sprint0.Task5.V0/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Tyuiu.NesterenkoVV.Sprint0.Task5.V0.Lib;
+
+namespace Tyuiu.NesterenkoVV.Sprint0.Task5.V0
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("A + B = " + DataService.Addition(1, 5));
+            Console.WriteLine("A - B = " + DataService.Substraction(15, 5));
+            Console.WriteLine("A * B = " + DataService.Multiplication(10, 5));
+            Console.WriteLine("A / B = " + DataService.Division(10, 0));
+            Console.ReadKey();
+        }
+    }
+}
